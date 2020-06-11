20ad4cde2a42443a0bc11872f963cff6ee29ee35
akshit sarin <akshitsarin99@gmail.com>
2020-06-11 09:30:38 +0530

    Real-Time video feedback displayed in the app

---
+++ screc.io/src/render.js
@@ -0,0 +1,49 @@
+// js document object model rules
+
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const selectVideo = document.getElementById("selectVideo");
+
+const vid = document.querySelector("video")
+const { desktopCapturer, remote } = require('electron');
+const { Menu } = remote;
+
+async function getVideo(){
+	const input = await desktopCapturer.getSources({
+		types:[
+			'window',
+			'screen'
+		]
+	});
+
+	const optionMenu = Menu.buildFromTemplate(
+		input.map(source => {
+			return {
+				label: source.name,
+				click: () => selectSource(source)
+			};
+		})
+	);
+
+	optionMenu.popup();
+}
+
+async function selectSource(source){
+	selectVideo.innerText = source.name;
+	const constraints = {
+		audio: false,
+		video: {
+			mandatory: {
+				chromeMediaSource: 'desktop',
+				chromeMediaSourceId: source.id
+			}
+		}
+	};
+	const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+	// add preview in app
+	vid.srcObject = stream;
+	vid.play();
+}
+
+selectVideo.onclick = getVideo;
